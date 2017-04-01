6a7866bdef21a13d7052d2b9567fd7b46354be94
Hye jun <vxggangxv@naver.com>
2017-04-01 12:31:09 +0900

    법인등기페이지제작

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,18 @@
 $(function() {
 
-  
+  //리뷰 슬라이드
+  $(document).ready(function(){
+    $('#review-slider').bxSlider({
+      /*auto: true,*/
+      slideWidth: 512,
+      minSlides: 2,
+      maxSlides: 2,
+      slideMargin: 10,
+      nextSelector: '#slider-next',
+      prevSelector: '#slider-prev',
+      nextText: '',
+      prevText: ''
+    });
+  });
 
 });
