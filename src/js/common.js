596c88f3f113a7ae28ffeefad1ae86033c2b51b3
Hye jun <vxggangxv@naver.com>
2017-04-11 19:04:35 +0900

    신청하기페이지

--- src/js/common.js
+++ src/js/common.js
@@ -1,15 +1,30 @@
-$(function() {
+$(function () {
   /*체크박스*/
-  $('.chk_box').click(function(){
-    if ($(this).prop('checked'))
-    {
+  $('.chk_box, .chk-box').click(function () {
+    if ($(this).prop('checked')) {
       $(this).addClass('on');
-    }else {
+    } else {
       $(this).removeClass('on');
     }
   });
   /*레이어팝업 닫기*/
-  $('.layer_pop .pop_area .pop_close').click(function(){
+  $('.layer_pop .pop_area .pop_close').click(function () {
     $(this).parents('.layer_pop').hide();
+  });
+  /*구매내역 탭메뉴*/
+  $('.mypage .buy_list h5').click(function () {
+    $(this).addClass('on').siblings('h5').removeClass('on');
+    $('.mypage .buy_list h5, .mypage .buy_list .cont').removeClass('on');
+    $(this).addClass('on').next().addClass('on');
+  });
+  /*아이디 비밀번호 찾기 탭메뉴*/
+  $('.find h4').click(function () {
+    $('.find h4, .find .cont').removeClass('on');
+    $(this).addClass('on').next().addClass('on');
+  });
+  /*이용약관 탭메뉴*/
+  $('.policy h3').click(function () {
+    $('.policy h3, .policy div').removeClass('on');
+    $(this).addClass('on').next().addClass('on');
   });
 });
---
+++ src/js/reviews/reviews.js
@@ -0,0 +1,13 @@
+$(function() {
+  //공지사항 리스트 페이지번호
+  $("#pagenation a").on("click", function() {
+    var idx = $(this).index();
+    var lastIdx = $(this).closest("div").find("a").length;
+    lastIdx -- ;
+    console.log(lastIdx);
+    if( idx != 0 && idx !=1 && idx != lastIdx && idx != lastIdx-1){
+      $(this).addClass("on").siblings().removeClass("on");
+    }
+    event.preventDefault();
+  });
+});
