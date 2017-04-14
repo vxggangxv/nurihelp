63d15fcb6d7df30d874d053084b282f04aa6dcf1
Hye jun <vxggangxv@naver.com>
2017-04-14 16:08:47 +0900

    법인등기사이트 완료

--- src/js/order/test/test.js
+++ src/js/order/test/test.js
@@ -1,6 +1,21 @@
 $(function() {
+  //선택 
   $("#choice li p").on("click", function() {
     $("#choice li p").removeClass("on");
     $(this).addClass("on");
+  });
+  //모두 동의 하기
+  $("#chk-all").on("click", function() {
+    var isChecked = $(this).prop("checked");
+    //console.log(isChecked);
+    if ($(this).prop('checked')) {
+      $(this).closest("div").find("input[type='checkbox']").addClass('on');
+    } else {
+      $(this).closest("div").find("input[type='checkbox']").removeClass('on');
+    }
+    $(this).closest("div").find("input[type='checkbox']").prop("checked", isChecked);
+    //console.log($("#chk1").prop("checked"));
+    //console.log($("#chk2").prop("checked"));
+    //console.log($("#chk3").prop("checked"));
   });
 });
